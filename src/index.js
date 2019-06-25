5ed5de5e651186c367ad8fd8e79c95feeac6fbf8
dwikiramdani <49061829+dwikiramdani@users.noreply.github.com>
2019-06-25 21:59:44 +0700

    Multiples of 3 and 5
    Complete function of Multiples of 3 and 5

--- src/index.js
+++ src/index.js
@@ -3,26 +3,20 @@
 
 function fibonacci(x) {
   var range = x;
-  var start = 0;
 
-  var a = 0;
-  var b = 1;
   var obj = [];
   var arr = 0;
 
-  var temp = 1;
+  var temp = 0;
 
-  while (start < range) {
-    temp = a + b;
-    a = b;
-    b = temp;
-    if (temp % 2 == 0) {
+  for (var n = 1; n <= range; n++) {
+    if (n % 3 === 0 || n % 5 === 0) {
       obj[arr] = temp;
+      temp = n;
       arr = arr + 1;
     }
-
-    start = start + 1;
   }
+
   console.log(range);
   console.log(obj);
 }
